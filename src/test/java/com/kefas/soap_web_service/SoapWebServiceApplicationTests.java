bb305a063dc536d10b4d58fc7bb183d85fdec577
Decagon <decagon@Decagons-MacBook-Air.local>
2023-02-15 17:13:23 +0100

    Initial commit

---
+++ src/test/java/com/kefas/soap_web_service/SoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefas.soap_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
